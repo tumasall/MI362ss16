82c0b69109ec18e52ce07c2c59c84b9a4fe1f43a
Allison Tumas <tumasall@msu.edu>
2016-02-10 17:28:08 -0500

    changed to html

--- week3/server.js
+++ week3/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('Hello World\n');
+  response.end('This is a node type\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
